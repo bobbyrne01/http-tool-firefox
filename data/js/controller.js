215717daf0c4cb65b187cbdb701ce403882ed57d
Robert Byrne <bobbyrne01@gmail.com>
2014-07-18 14:53:49 +0100

    #2 Can set headers on requests

--- data/js/controller.js
+++ data/js/controller.js
@@ -1,20 +1,32 @@
 // listen for click events on UI
 window.addEventListener('click', function(event) {
 	
-	if (event.target.id.indexOf('submit') == 0){
+	if (event.target.id.indexOf('submit') === 0){
+		
+		var headers = {};
+		
+		// Identify if headers need to be sent in request
+		for (var i = 1, row; row = document.getElementById("headersRequestTable").rows[i]; i++) {
+			
+			if (!(row.cells[0].firstElementChild.value === '' || row.cells[1].firstElementChild.value === '')){
+				headers[row.cells[0].firstElementChild.value] = row.cells[1].firstElementChild.value;
+			}	
+		}
+		
 		
 		var input = JSON.stringify({
 			operation: 'submit',
 			query: JSON.stringify({
 				url: document.getElementById("url").value,
 				method: document.getElementById("method").options[document.getElementById("method").selectedIndex].text,
-				content: document.getElementById("bodyRequestListItem").value
+				content: document.getElementById("bodyRequestListItem").value,
+				headers: headers
 			})
 	    });
 		
 		self.postMessage(input);
 		
-	} else if (event.target.id.indexOf('reset') == 0){
+	} else if (event.target.id.indexOf('reset') === 0){
 		
 		document.getElementById("method").selectedIndex = 0;
 		document.getElementById("url").value = ""
@@ -23,8 +35,32 @@ window.addEventListener('click', function(event) {
 		document.getElementById("status").textContent = "";
 		document.getElementById("statusText").textContent = "";
 		document.getElementById("statusListItem").style.backgroundColor = '#F6F6F9';
-	}
 	
+	}else if (event.target.id.indexOf('newHeaderButton') === 0){
+		
+		var tr = document.createElement("tr"),
+			tdName = document.createElement("td"),
+			tdValue = document.createElement("td"),
+			inputName = document.createElement("input"),
+			inputValue = document.createElement("input"),
+			inputButton = document.createElement("input");
+		
+		inputName.length = 25;
+		inputValue.length = 25;
+		inputButton.length = 25;
+		inputButton.type = "button";
+		inputButton.value = "-";
+		inputButton.onclick = function(){
+			document.getElementById('headersRequestTable').deleteRow(this.parentNode.rowIndex);
+		};
+		
+		tdName.appendChild(inputName);
+		tdValue.appendChild(inputValue);
+		tr.appendChild(tdName);
+		tr.appendChild(tdValue);
+		tr.appendChild(inputButton);
+		document.getElementById("headersRequestTable").appendChild(tr);
+	}
 }, false);
 
 
@@ -47,7 +83,7 @@ self.port.on("response", function(responseString) {
 		document.getElementById("statusListItem").style.backgroundColor = '#99FF66';
 	}else if (response.status >= 300 && response.status <= 399){
 		document.getElementById("statusListItem").style.backgroundColor = '#FFFF66';
-	}else if (response.status >= 400 && response.status <= 499 || response.status == 0){
+	}else if (response.status >= 400 && response.status <= 499 || response.status === 0){
 		document.getElementById("statusListItem").style.backgroundColor = '#FF3333';
 	}else if (response.status >= 500 && response.status <= 599){
 		document.getElementById("statusListItem").style.backgroundColor = '#CC3333';
