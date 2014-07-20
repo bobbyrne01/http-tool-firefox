54b166ef9c95b2954295d6d78de1a18c23d28980
Robert Byrne <bobbyrne01@gmail.com>
2014-07-20 23:39:47 +0100

    Changed `url` element to input and added keylistener to `url` for enter presses

--- data/js/controller.js
+++ data/js/controller.js
@@ -3,65 +3,28 @@ window.addEventListener('click', function(event) {
 	
 	if (event.target.id.indexOf('submit') === 0){
 		
-		var headers = {};
-		
-		// Identify if headers need to be sent in request
-		for (var i = 1, row; row = document.getElementById("headersRequestTable").rows[i]; i++) {
-			
-			if (!(row.cells[0].firstElementChild.value === '' || row.cells[1].firstElementChild.value === '')){
-				headers[row.cells[0].firstElementChild.value] = row.cells[1].firstElementChild.value;
-			}	
-		}
-		
-		
-		var input = JSON.stringify({
-			operation: 'submit',
-			query: JSON.stringify({
-				url: document.getElementById("url").value,
-				method: document.getElementById("method").options[document.getElementById("method").selectedIndex].text,
-				content: document.getElementById("bodyRequestListItem").value,
-				headers: headers
-			})
-	    });
-		
-		self.postMessage(input);
+		httprequester.submit();
 		
 	} else if (event.target.id.indexOf('reset') === 0){
 		
-		document.getElementById("method").selectedIndex = 0;
-		document.getElementById("url").value = ""
-		document.getElementById("headers").textContent = "";
-		document.getElementById("body").textContent = "";
-		document.getElementById("status").textContent = "";
-		document.getElementById("statusText").textContent = "";
-		document.getElementById("statusListItem").style.backgroundColor = '#F6F6F9';
+		httprequester.reset();
 	
 	}else if (event.target.id.indexOf('newHeaderButton') === 0){
 		
-		var tr = document.createElement("tr"),
-			tdName = document.createElement("td"),
-			tdValue = document.createElement("td"),
-			inputName = document.createElement("input"),
-			inputValue = document.createElement("input"),
-			inputButton = document.createElement("input");
-		
-		inputName.length = 25;
-		inputValue.length = 25;
-		inputButton.length = 25;
-		inputButton.type = "button";
-		inputButton.value = "-";
-		inputButton.onclick = function(){
-			document.getElementById('headersRequestTable').deleteRow(this.parentNode.rowIndex);
-		};
-		
-		tdName.appendChild(inputName);
-		tdValue.appendChild(inputValue);
-		tr.appendChild(tdName);
-		tr.appendChild(tdValue);
-		tr.appendChild(inputButton);
-		document.getElementById("headersRequestTable").appendChild(tr);
+		httprequester.addHeader();
 	}
 }, false);
+
+
+// listen for enter pressed on `url` input
+document.getElementById('url').onkeypress = function(e){
+	if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+    
+    	httprequester.submit();
+    }
+}
 
 
 // Populate Response area with data
@@ -101,6 +64,72 @@ self.port.on("response", function(payload) {
 	}
 });
 
+
 self.port.on("error", function(payload) {
 	document.getElementById("statusListItem").style.backgroundColor = '#FF3333';
 });
+
+
+var httprequester = {
+		
+	submit: function() {
+		var headers = {};
+
+		// Identify if headers need to be sent in request
+		for (var i = 1, row; row = document.getElementById("headersRequestTable").rows[i]; i++) {
+			
+			if (!(row.cells[0].firstElementChild.value === '' || row.cells[1].firstElementChild.value === '')){
+				headers[row.cells[0].firstElementChild.value] = row.cells[1].firstElementChild.value;
+			}	
+		}
+
+
+		var input = JSON.stringify({
+			operation: 'submit',
+			query: JSON.stringify({
+				url: document.getElementById("url").value,
+				method: document.getElementById("method").options[document.getElementById("method").selectedIndex].text,
+				content: document.getElementById("bodyRequestListItem").value,
+				headers: headers
+			})
+		});
+
+		self.postMessage(input);
+	},
+	
+	reset: function() {
+		document.getElementById("method").selectedIndex = 0;
+		document.getElementById("url").value = ""
+		document.getElementById("headers").textContent = "";
+		document.getElementById("body").textContent = "";
+		document.getElementById("status").textContent = "";
+		document.getElementById("statusText").textContent = "";
+		document.getElementById("statusListItem").style.backgroundColor = '#F6F6F9';
+	},
+	
+	
+	addHeader: function() {
+		var tr = document.createElement("tr"),
+			tdName = document.createElement("td"),
+			tdValue = document.createElement("td"),
+			inputName = document.createElement("input"),
+			inputValue = document.createElement("input"),
+			inputButton = document.createElement("input");
+		
+		inputName.length = 25;
+		inputValue.length = 25;
+		inputButton.length = 25;
+		inputButton.type = "button";
+		inputButton.value = "-";
+		inputButton.onclick = function(){
+			document.getElementById('headersRequestTable').deleteRow(this.parentNode.rowIndex);
+		};
+		
+		tdName.appendChild(inputName);
+		tdValue.appendChild(inputValue);
+		tr.appendChild(tdName);
+		tr.appendChild(tdValue);
+		tr.appendChild(inputButton);
+		document.getElementById("headersRequestTable").appendChild(tr);
+	}
+}
